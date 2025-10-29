5b692a2ec6b92fa0b38e8602dfed8480b5ad29ff
Minh Phung <phungminhby@gmail.com>
2025-10-29 10:37:33 +0700

    tổ chức file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/globals.scss';
+import App from './views/App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
